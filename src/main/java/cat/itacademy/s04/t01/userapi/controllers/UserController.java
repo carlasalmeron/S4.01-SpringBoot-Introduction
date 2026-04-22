262a830ac0d7209c81487de8fa1cee13aea454a5
Carla Salmerón <salmeroncarla98@gmail.com>
2026-04-22 12:54:49 +0200

    feat: implement basic UserController with empty list and GET endpoint

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -0,0 +1,21 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+import cat.itacademy.s04.t01.userapi.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping ("/users")
+public class UserController {
+
+    private static final List<User> users = new ArrayList<>();
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+}
