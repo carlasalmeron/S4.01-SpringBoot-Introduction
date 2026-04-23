c35047f76013e630f5a0a91b5a226a54ae29bbb9
Carla Salmerón <salmeroncarla98@gmail.com>
2026-04-23 11:14:58 +0200

    feat: implement POST /users to create users with UUID

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,21 +1,27 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 @RequestMapping ("/users")
 public class UserController {
 
-    private static final List<User> users = new ArrayList<>();
+    private static final List<User> users = new CopyOnWriteArrayList<>();
 
     @GetMapping
     public List<User> getAllUsers() {
         return users;
+    }
+
+    @PostMapping
+    public User createUser (@RequestBody User user) {
+        user.setId(UUID.randomUUID());
+        users.add(user);
+        return user;
     }
 
 }
